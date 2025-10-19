c6e1af6b57b36cd2eae94f156c732b451b49688b
Yohan Garcia <yoanvaldes03@gmail.com>
2025-10-19 12:19:26 -0400

    - removed the call from axios

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,19 @@
-<script>
-import Navbar from './components/NavBar.vue';
-import Footer from './components/Footer.vue';
-import authState from './stores/auth';
-import axiosClient from './util/axiosClient';
-
-export default {
-  name: 'App',
-  components: {
-    Navbar,
-    Footer,
-  },
-  data() {
-    return {
-      authState,
-    };
-  },
-  created() {
-    this.checkAuth();
-  },
-  methods: {
-    async checkAuth() {
-      try {
-        const apiKey = import.meta.env.VITE_API_KEY;
-        const res = await axiosClient.get('auth/me', {
-          headers: { 'x-api-key': apiKey },
-          withCredentials: true,
-        });
-        this.authState.isAuthenticated = res.status === 200;
-      } catch (err) {
-        this.authState.isAuthenticated = false;
-      }
-    },
-  },
-};
+<script setup>
+import Navbar from './components/NavBar.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
-  <Navbar />
+  <Navbar></Navbar>
   <main class="p-2">
-    <RouterView />
+    <RouterView to="/" />
   </main>
-  <Footer />
+  <Footer></Footer>
 </template>
 
 <style scoped>
-main {
-  min-height: 100vh;
-  background-color: rgba(241, 240, 240, 0.5);
-}
+  main {
+    min-height: 100vh;
+    background-color: rgba(241, 240, 240, 0.5);
+  }
 </style>
