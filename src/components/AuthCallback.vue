72698cd6b3490e592466435b74148c6a6da88524
Yohan Garcia <yoanvaldes03@gmail.com>
2025-10-18 23:40:49 -0400

    - updating the authcallback by removing the session jwt token for a more secure and streamline process

--- src/components/AuthCallback.vue
+++ src/components/AuthCallback.vue
@@ -3,15 +3,28 @@
 </template>
 
 <script>
-export default {
-  mounted() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const token = urlParams.get("token");
+import axiosClient from "../util/axiosClient";
 
-    if (token) {
-      sessionStorage.setItem("jwt", token);
-      this.$router.push("/");
-    } else {
+export default {
+  async mounted() {
+    try {
+      const apiKey = import.meta.env.VITE_API_KEY;
+
+      const res = await axiosClient.get("auth/me", {
+        headers: {
+          "x-api-key": apiKey,
+        },
+      });
+
+      // If it succeeds (JWT cookie is valid)
+      if (res.status === 200) {
+        console.log("User authenticated:", res.data);
+        this.$router.push("/"); // Go to home
+      } else {
+        this.$router.push("/login"); // Not logged in
+      }
+    } catch (err) {
+      console.warn("Not authenticated or token expired:", err);
       this.$router.push("/login");
     }
   }
